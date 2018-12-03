f15ef496bf544e031155fe09b16695106989a76a
meron93 <meronyemane18@gmail.com>
2018-12-02 17:07:50 -0800

    Header and footer created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    Hello I am an Angular app.
+    <!--- header --->
+    <app-header></app-header>
+    
+    <!--- routes get injected here --->
     <router-outlet></router-outlet>
+
+    <!--- footer --->
+    <app-footer></app-footer>
   `,
   styles: []
 })
